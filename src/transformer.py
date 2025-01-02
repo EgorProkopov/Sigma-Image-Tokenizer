4f02ec57e9fa7810ff2513f30b77aa1f810adeb0
Egor Prokopov <egorprokopov216@gmail.com>
2025-01-02 23:37:11 +0300

    add vit transformer

--- src/transformer.py
+++ src/transformer.py
@@ -122,7 +122,91 @@ class TransformerEncoder(nn.Module):
         return x
 
 
-class TransformerClassifierModel(nn.Module):
+class PatchProjection(nn.Module):
+    def __init__(
+        self,
+        patch_size: int = 16,
+        in_channels: int = 3,
+        embed_dim: int = 64,
+	):
+        super().__init__()
+        self.embed_dim = embed_dim
+        self.patch_size = patch_size
+
+        self._embedder = nn.Conv2d(in_channels, embed_dim, kernel_size=patch_size, stride=patch_size)
+
+    def forward(self, tensor):
+        x = self._embedder(tensor)
+        x = x.flatten(2)  # [batch_size, embed_dim, num_patches]
+        x = x.transpose(1, 2)  # [batch_size, num_patches, embed_dim]
+        return x
+
+
+class ViTTokenization(nn.Module):
+    def __init__(
+        self,
+        image_size: int = 224,
+        patch_size: int = 16,
+        in_channels: int = 3,
+        embed_dim: int = 768,
+    ):
+        super().__init__()
+        self.patch_embedder = PatchProjection(patch_size=patch_size, in_channels=in_channels, embed_dim=embed_dim)
+        num_patches = (image_size // patch_size) ** 2
+
+        self.pos_embeddings = nn.Parameter(torch.randn(1, num_patches + 1, embed_dim))
+
+        self.cls_token = nn.Parameter(torch.randn(1, 1, embed_dim))
+
+    def forward(self, tensor):
+        x = self.patch_embedder(tensor)
+        batch_size = x.size(0)
+        cls_tokens = self.cls_token.expand(batch_size, -1, -1)
+        x = torch.cat((cls_tokens, x), dim=1)
+        x = x + self.pos_embeddings
+        return x
+
+
+class VisionTransformer(nn.Module):
+    def __init__(
+        self,
+        image_size: int = 224,
+        patch_size: int = 16,
+        in_channels: int = 3,
+        embed_dim: int = 768,
+        qkv_dim: int = 64,
+        mlp_hidden_size: int = 3072,
+        n_layers: int = 12,
+        n_heads: int = 12,
+        n_classes: int = 200,
+    ):
+        super().__init__()
+
+        self.tokenization = ViTTokenization(
+            image_size=image_size,
+            patch_size=patch_size,
+            in_channels=in_channels,
+            embed_dim=embed_dim
+        )
+
+        self.transformer_encoder = TransformerEncoder(
+            embed_dim=embed_dim,
+            qkv_dim=qkv_dim,
+            mlp_hidden_size=mlp_hidden_size,
+            n_layers=n_layers,
+            n_heads=n_heads
+        )
+        self.classifier = nn.Linear(embed_dim, n_classes)
+
+    def forward(self, tensor):
+        x = self.tokenization(tensor)
+        x = self.transformer_encoder(x)
+        x = x[:, 0]
+        logits = self.classifier(x)
+        return logits
+
+
+class SVDViT(nn.Module):
     def __init__(
         self,
         image_size: int = 224,
@@ -136,7 +220,6 @@ class TransformerClassifierModel(nn.Module):
     ):
         super().__init__()
 
-        # Image Tokenizer
         self.tokenizer = None
         self.transformer_encoder = TransformerEncoder(
             embed_dim=embed_dim,
