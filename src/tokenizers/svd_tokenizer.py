96145b5f8394545471c82731d887ec07a77e39e3
Egor Prokopov <egorprokopov216@gmail.com>
2025-03-31 05:22:38 +0300

    add embedder layer to svd tokenizer

--- src/tokenizers/svd_tokenizer.py
+++ src/tokenizers/svd_tokenizer.py
@@ -3,12 +3,14 @@ import torch.nn as nn
 
 
 class SVDTokenizer(nn.Module):
-    def __init__(self, dispersion=0.9):
+    def __init__(self, image_size, embedding_size, dispersion=0.9):
         super().__init__()
 
         self.dispersion = dispersion
 
-    def forward(self, image: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, int]:
+        self.embedder = nn.Linear(image_size * 2, embedding_size)
+
+    def get_approx_svd(self, image: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, int]:
         U, S, Vh = torch.linalg.svd(image, full_matrices=True)
         V = Vh.mH
 
@@ -31,9 +33,37 @@ class SVDTokenizer(nn.Module):
 
         return approx_U, approx_S, approx_V, rank
 
+    def forward(self, image: torch.Tensor) -> torch.Tensor:
+        # with torch.no_grad():
+        # Shapes: [channels,image_size, rank] and [channels, rank]
+        approx_U, approx_S, approx_V, _ = self.get_approx_svd(image)
+
+        approx_U = approx_U.transpose(1, 2)
+        approx_V = approx_V.transpose(1, 2)
+
+        approx_S_root = torch.sqrt(approx_S)
+
+        # print(approx_U.shape)
+        # print(approx_S_root.shape)
+        # print(approx_V.shape)
+
+        approx_US = approx_U * approx_S_root.unsqueeze(-1)  # Shape: [channels, rank, image_size]
+        approx_SV = approx_V * approx_S_root.unsqueeze(-1)  # Shape: [channels, rank, image_size]
+
+        approx_US = approx_US.transpose(0, 1)  # Shape: [rank, channels, image_size]
+        approx_SV = approx_SV.transpose(0, 1)  # Shape: [rank, channels, image_size]
+
+        raw_embedding = torch.cat([approx_US, approx_SV], dim=-1)  # Shape: [rank, channels, image_size * 2]
+        channels, rank, image_size = raw_embedding.shape
+        raw_embedding = torch.reshape(raw_embedding, (channels * rank, image_size))
+
+        embedding = self.embedder(raw_embedding)
+        return embedding
+
+
     @staticmethod
     def reconstruct_image(approx_U, approx_S, approx_V):
-        US = approx_U * approx_S.unsqueeze(1)  # Shape: [channels, m, rank]
+        US = approx_U * approx_S.unsqueeze(1)  # Shape: [channels, image_size, rank]
         Vt = approx_V.transpose(1, 2)
         approx_image = torch.bmm(US, Vt)
 
